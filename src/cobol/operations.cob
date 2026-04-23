1dae20d1f51b25fd3d2292613ec8bda10aa4bf5b
JhonyVargas <johnnyvargasluque@gmail.com>
2026-04-22 22:19:16 -0500

    Refactorizado código para mejorar la eficiencia y legibilidad

--- src/cobol/operations.cob
+++ src/cobol/operations.cob
@@ -10,31 +10,37 @@
        LINKAGE SECTION.
        01 PASSED-OPERATION   PIC X(6).
 
+       
        PROCEDURE DIVISION USING PASSED-OPERATION.
            MOVE PASSED-OPERATION TO OPERATION-TYPE
+           PERFORM READ-BALANCE
 
-           IF OPERATION-TYPE = 'TOTAL '
-               CALL 'DataProgram' USING 'READ', FINAL-BALANCE
-               DISPLAY "Current balance: " FINAL-BALANCE
-
-           ELSE IF OPERATION-TYPE = 'CREDIT'
-               DISPLAY "Enter credit amount: "
-               ACCEPT AMOUNT
-               CALL 'DataProgram' USING 'READ', FINAL-BALANCE
-               ADD AMOUNT TO FINAL-BALANCE
-               CALL 'DataProgram' USING 'WRITE', FINAL-BALANCE
-               DISPLAY "Amount credited. New balance: " FINAL-BALANCE
-
-           ELSE IF OPERATION-TYPE = 'DEBIT '
-               DISPLAY "Enter debit amount: "
-               ACCEPT AMOUNT
-               CALL 'DataProgram' USING 'READ', FINAL-BALANCE
-               IF FINAL-BALANCE >= AMOUNT
-                   SUBTRACT AMOUNT FROM FINAL-BALANCE
-                   CALL 'DataProgram' USING 'WRITE', FINAL-BALANCE
-                   DISPLAY "Amount debited. New balance: " FINAL-BALANCE
-               ELSE
-                   DISPLAY "Insufficient funds for this debit."
-               END-IF
-           END-IF
+           EVALUATE OPERATION-TYPE
+               WHEN 'TOTAL '
+                   DISPLAY "Current balance: " FINAL-BALANCE
+               WHEN 'CREDIT'
+                   DISPLAY "Enter credit amount: "
+                   ACCEPT AMOUNT
+                   ADD AMOUNT TO FINAL-BALANCE
+                   PERFORM WRITE-BALANCE
+                   DISPLAY "Amount credited. New balance: " FINAL-BALANCE
+               WHEN 'DEBIT '
+                   DISPLAY "Enter debit amount: "
+                   ACCEPT AMOUNT
+                   IF FINAL-BALANCE >= AMOUNT
+                       SUBTRACT AMOUNT FROM FINAL-BALANCE
+                       PERFORM WRITE-BALANCE
+                       DISPLAY "Amount debited. New balance: " FINAL-BALANCE
+                   ELSE
+                       DISPLAY "Insufficient funds for this debit."
+                   END-IF
+               WHEN OTHER
+                   DISPLAY "Invalid operation type."
+           END-EVALUATE
            GOBACK.
+
+       READ-BALANCE.
+           CALL 'DataProgram' USING 'READ', FINAL-BALANCE.
+
+       WRITE-BALANCE.
+           CALL 'DataProgram' USING 'WRITE', FINAL-BALANCE.
